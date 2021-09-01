eba3b541751d8d9a3a1860940061487d2b835e5f
demodeveloper-at-488942835940 <supradipmajumdar@yahoo.com>
2021-09-01 20:30:40 +0530

    comment 1st Sept 2021

--- src/main/java/training/com/training1/TrainingController.java
+++ src/main/java/training/com/training1/TrainingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
 /**
  *
- * @author manoj.bardhan
+ * @author supradip
  *
  */
 @RestController
