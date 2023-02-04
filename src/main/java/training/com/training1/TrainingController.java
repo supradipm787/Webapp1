de0c71510aac379d653480cf6476cf23510e8870
supradipm787 <33974683+supradipm787@users.noreply.github.com>
2023-02-04 17:25:24 +0530

    Update TrainingController.java

--- src/main/java/training/com/training1/TrainingController.java
+++ src/main/java/training/com/training1/TrainingController.java
@@ -16,12 +16,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TrainingController {
 @RequestMapping("/devtraining")
 public String index() {
-return "Hello - training for Developer 28th May !!!";
+return "Hello - training for Developer by 28th Feb 2023 !!!";
 }
 
 @RequestMapping("/scrumtraining")
 public String scrumtraining() {
-return "Hello - training for Scrum Masters  1st June !!!";
+return "Hello - training for Scrum Masters by 30th April 2023 !!!";
 }  
   
 }
