fdbf1a98dacb4db324e75696195ca62782f91e12
supradipm787 <supradipm7@gmail.com>
2026-06-21 10:31:16 +0530

    Modifying print

--- src/main/java/training/com/training1/TrainingController.java
+++ src/main/java/training/com/training1/TrainingController.java
@@ -16,12 +16,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TrainingController {
 @RequestMapping("/devtraining")
 public String index() {
-return "GD & training for Developers of Cybage on Domain Driven design & Microservice Architecture by 18th Feb 2023 !!!!!!!!";
+return "GD & training for Developers of Cybage on Domain Driven design & Microservice Architecture by 18th Feb 2023";
 }
 
 @RequestMapping("/scrumtraining")
 public String scrumtraining() {
-return "GD & training on Scrum Master & Product Owner to Project Managers of Cybage by 1st June 2023!!!!!!";
+return "GD & training on Scrum Master & Product Owner to Project Managers of Cybage by 1st June 2023";
 }  
   
 }
